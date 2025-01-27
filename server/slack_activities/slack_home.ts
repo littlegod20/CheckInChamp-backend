f9b4e877faab3f83418c6b41d66704d1ddf7ca0b
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-01-27 00:20:58 +0000

    replaced mongo uri with new uri and changed app name from flowsync to checkInChamp

--- server/slack_activities/slack_home.ts
+++ server/slack_activities/slack_home.ts
@@ -1,59 +1,45 @@
 import { slackApp } from "../config/slack";
 
-export const home_pub = () =>{
-    // Add event listener for app_home_opened events
-    slackApp.event("app_home_opened", async ({ event, client }) => {
-        try {
-          await client.views.publish({
-            user_id: event.user,
-            view: {
-              type: "home",
-              callback_id: "home_view",
-              blocks: [
+export const home_pub = () => {
+  // Add event listener for app_home_opened events
+  slackApp.event("app_home_opened", async ({ event, client }) => {
+    try {
+      await client.views.publish({
+        user_id: event.user,
+        view: {
+          type: "home",
+          callback_id: "home_view",
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "Welcome to *Check In Champ*! 🎉",
+              },
+            },
+            {
+              type: "divider",
+            },
+
+            {
+              type: "actions",
+              elements: [
                 {
-                    type: "section",
-                    text: {
-                      type: "mrkdwn",
-                      text: "Welcome to *FlowSync*! 🎉\n\nFlowSync is here to transform how you work. Customize and configure your bot with ease using our web-based interface. Create personalized workflows, automate tasks, and unlock your team's productivity. Let's sync and flow together!"
-                    }
-                  },
-                {
-                  type: "divider"
-                },
-                {
-                  type: "section",
+                  type: "button",
                   text: {
-                    type: "mrkdwn",
-                    text: "Here are some quick actions:"
-                  }
+                    type: "plain_text",
+                    text: "Manage Your Teams",
+                  },
+                  url: "http://localhost:5173/",
                 },
-                {
-                  type: "actions",
-                  elements: [
-                    {
-                      type: "button",
-                      text: {
-                        type: "plain_text",
-                        text: "Read about FlowSync"
-                      },
-                      "url": "http://localhost:5173/"
-                    },
-                    {
-                      type: "button",
-                      text: {
-                        type: "plain_text",
-                        text: "Get Started"
-                      },
-                      "url": "http://localhost:5173/"
-                    }
-                  ]
-                }
-              ]
-            }
-          });
-        } catch (error) {
-          console.error("Error publishing App Home:", error);
-        }
+              ],
+            },
+
+          ],
+        },
       });
-      
-}
+    } catch (error) {
+      console.error("Error publishing App Home:", error);
+    }
+  });
+};
